7e9722452efa71e43d2b26b5ccfbcfdd4efa33ff
Sumanta Das <sumantad827@gmail.com>
2022-11-19 13:02:39 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,11 @@
+# write a python program to calculate rectangle area
+
+userinput1 = float(input("Enter Rectangle length : "))
+userinput2 = float(input("Enter Rectangle breadth : "))
+
+Area=userinput1*userinput2
+
+print('you enter length : ',userinput1)
+print('you enter breadth : ',userinput2)
+print("Rectangle Area is : ",Area)
+
